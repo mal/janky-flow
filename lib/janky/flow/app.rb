1ef5655b7fdd11e2317d81e8f4b42a912b35e558
Mal Graty <mal.graty@googlemail.com>
2013-11-08 13:18:40 +0000

    Use middleware to correctly capture routes

--- lib/janky/flow/app.rb
+++ lib/janky/flow/app.rb
@@ -1,11 +1,17 @@
 module Janky
-  class App < Sinatra::Base
+  module Flow
+    class App < Sinatra::Base
 
-    get %r{\/\*\/([-_\+\.a-zA-z0-9\/]+)} do |branch_name|
-      authorize_index
-      @builds = Build.queued.where(:branch_id => Branch.where(:name => branch_name)).first(50)
-      mustache :index
+      get %r{\/\*\/([-_\+\.a-zA-z0-9\/]+)} do |branch_name|
+        authorize_index
+        @builds = Build.queued.where(:branch_id => Branch.where(:name => branch_name)).first(50)
+        mustache :index
+      end
+
     end
+  end
 
+  class App
+    use Janky::Flow::App
   end
 end
--- lib/janky/flow/hubot.rb
+++ lib/janky/flow/hubot.rb
@@ -1,24 +1,35 @@
 module Janky
-  class Hubot < Sinatra::Base
+  module Flow
+    class Hubot < Sinatra::Base
 
-    get %r{\/\*\/([-_\+\.a-zA-z0-9\/]+)} do |branch_name|
-      branch_summary branch_name
-    end
-
-    def branch_summary(branch_name)
-      content_type "text/plain"
-      repos = Repository.all(:include => [:branches, :commits, :builds]).map do |repo|
-        branch = repo.branch_for(branch_name)
-
-        "%-17s %-13s %-10s %40s" % [
-          repo.name,
-          branch.status,
-          repo.campfire_room,
-          repo.uri
-        ]
+      get '/help' do
+        fail "downstream app not set" unless @app.respond_to? :call
+        status, headers, body = @app.call env
+        @response.status = status
+        @response.body = body.body[0].sub! "ci status\n", "\\0ci status */develop\n"
+        @response.headers.merge! headers.reject { |k| k == 'Content-Length' }
+        nil
       end
-      repos.join("\n")
-    end
 
+      get %r{\/\*\/([-_\+\.a-zA-z0-9\/]+)} do |branch_name|
+        content_type "text/plain"
+        repos = Repository.all(:include => [:branches, :commits, :builds]).map do |repo|
+          branch = repo.branch_for(branch_name)
+
+          "%-17s %-13s %-10s %40s" % [
+            repo.name,
+            branch.status,
+            repo.campfire_room,
+            repo.uri
+          ]
+        end
+        repos.join("\n")
+      end
+
+    end
+  end
+
+  class Hubot
+    use Janky::Flow::Hubot
   end
 end
